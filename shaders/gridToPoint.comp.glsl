d18db2250a3f5d8d79c36eb495b57dc1fa44b5ce
jb <jb239812@ohio.edu>
2026-04-13 09:46:17 -0400

    shader bindings

--- shaders/gridToPoint.comp.glsl
+++ shaders/gridToPoint.comp.glsl
@@ -3,9 +3,21 @@
 #extension GL_GOOGLE_include_directive : require
 #extension GL_EXT_buffer_reference : require
 
-layout ( local_size_x = 16, local_size_y = 16 ) in;
+layout ( local_size_x = 64 ) in;
 
 #include "common.h"
+
+struct point {
+	vec2 position;
+	vec2 velocity;
+};
+layout ( set = 0, binding = 1, std430 ) buffer pointBuffer {
+	point points[];
+};
+
+layout ( r32i, set = 0, binding = 2 ) uniform iimage2D velocityXAtomic;
+layout ( r32i, set = 0, binding = 3 ) uniform iimage2D velocityYAtomic;
+layout ( r32i, set = 0, binding = 4 ) uniform iimage2D massAtomic;
 
 void main () {
 
--- shaders/updateGrid.comp.glsl
+++ shaders/updateGrid.comp.glsl
@@ -7,6 +7,25 @@ layout ( local_size_x = 16, local_size_y = 16 ) in;
 
 #include "common.h"
 
-void main () {
+struct point {
+	vec2 position;
+	vec2 velocity;
+};
+layout ( set = 0, binding = 1, std430 ) buffer pointBuffer {
+	point points[];
+};
 
+layout ( r32i, set = 0, binding = 2 ) uniform iimage2D velocityXAtomic;
+layout ( r32i, set = 0, binding = 3 ) uniform iimage2D velocityYAtomic;
+layout ( r32i, set = 0, binding = 4 ) uniform iimage2D massAtomic;
+
+void main () {
+	ivec2 loc = ivec2( gl_GlobalInvocationID.xy );
+	ivec2 size = ivec2( imageSize( massAtomic ).xy );
+	// bounds checking
+	if ( all( lessThan( loc, size ) ) ) {
+		// imageStore( velocityXAtomic, loc, ivec4( 0 ) );
+		// imageStore( velocityYAtomic, loc, ivec4( 0 ) );
+		// imageStore( massAtomic, loc, ivec4( 0 ) );
+	}
 }
